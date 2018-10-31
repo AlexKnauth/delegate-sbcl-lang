a9694adaf5af7bd276a07fbb3a1a175523be8225
AlexKnauth <alexander@knauth.org>
2018-10-30 22:26:25 -0400

    add #lang delegate/acl2

---
+++ delegate/acl2.rkt
@@ -0,0 +1,37 @@
+#lang racket/base
+
+(provide (rename-out [module-begin #%module-begin]))
+
+(require racket/system
+         racket/port
+         racket/file
+         syntax/parse/define)
+
+(define-simple-macro (module-begin form:expr ...)
+  (#%module-begin
+   (delegate-acl2/repl '(form ...))))
+
+(define acl2
+  #;(find-executable-path "acl2")
+  #;(with-output-to-string (λ () (system "which acl2")))
+  #;(with-output-to-string (λ () (system "bash -c 'which acl2'")))
+  "$HOME/Documents/acl2/acl2/saved_acl2")
+
+;; delegate-acl2/repl : [Listof S-Expr] -> Void
+(define (delegate-acl2/repl forms)
+  (define-values [tmp-in tmp-out] (make-pipe))
+  (write-forms forms tmp-out)
+  (close-output-port tmp-out)
+  (define appended-in
+    (input-port-append #true tmp-in (current-input-port)))
+  (define command
+    (format "~v" acl2))
+  (void (parameterize ([current-input-port appended-in])
+          (system command))))
+
+;; write-forms : [Listof S-Expr] OutputPort -> Void
+(define (write-forms forms out)
+  (for ([form (in-list forms)])
+    (writeln form out)))
+
+

c5636ae8f02ea43c411ea09df28bdeedd3563ad4
AlexKnauth <alexander@knauth.org>
2018-11-01 16:58:03 -0400

    read the Json value that cedille puts out

--- delegate/cedille.rkt
+++ delegate/cedille.rkt
@@ -7,6 +7,7 @@
          racket/system
          racket/port
          syntax/parse/define
+         json
          "util/which.rkt"
          "util/provide-unprefix.rkt"
          (prefix-in acl2s: "util/scope.rkt")
@@ -32,11 +33,30 @@
   (define tmp-out (open-output-file tmp #:exists 'replace))
   (write-string s tmp-out)
   (close-output-port tmp-out)
+  ;; ----------
+  (define-values [my-in their-out] (make-pipe))
   (define command
     (format "~v" cedille))
   (define in
     (open-input-string
      (format "check§~a\n" (path->string tmp))))
-  (void (parameterize ([current-input-port in])
-          (system command))))
+  (void (parameterize ([current-input-port in]
+                       [current-output-port their-out])
+          (system command)))
+  ;; get rid of lines that start with "progress: "
+  (consume-progress-lines my-in)
+  ;; read a JSON value
+  (read-json my-in))
 
+;; consume-progress-lines : InputPort -> Void
+;; get rid of lines that start with "progress: "
+(define (consume-progress-lines in)
+  (define progress-start "progress: ")
+  (define would-be-progress (peek-string (string-length progress-start) 0 in))
+  (cond
+    [(and (string? would-be-progress)
+          (string=? would-be-progress progress-start))
+     (read-line in)
+     (consume-progress-lines in)]
+    [else
+     (void)]))

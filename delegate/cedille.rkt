18b3f2a7618ebaaa6c6bafdc8f9885f839ca4153
AlexKnauth <alexander@knauth.org>
2018-11-01 19:10:24 -0400

    start thinking a bit about mouse-over-tooltips for cedille

--- delegate/cedille.rkt
+++ delegate/cedille.rkt
@@ -9,10 +9,13 @@
          syntax/parse/define
          json
          "cedille/check.rkt"
-         "util/which.rkt"
          "util/write-whitespace-toward.rkt"
          (for-syntax racket/base
-                     syntax/parse))
+                     #;racket/file
+                     #;racket/match
+                     syntax/parse
+                     #;"cedille/check.rkt"
+                     #;"util/write-whitespace-toward.rkt"))
 (module+ test
   (require rackunit))
 
@@ -20,14 +23,12 @@
 
 (define-simple-macro (module-begin src ln:nat col:nat pos:nat s:str)
   (#%module-begin
+   ;(mouse-over-tooltips src ln col pos s)
    (delegate-cedille 'src 'ln 'col 'pos 's)))
 
 ;; -----------------------------------------------
 
 ;; Delagating to Cedille
-
-(define cedille
-  (which "cedille"))
 
 ;; delegate-cedille : Any Nat Nat Nat String -> Void
 (define (delegate-cedille src ln col pos s)
@@ -41,6 +42,52 @@
     (cedille-check tmp))
   ;; handle the JSON value
   (handle-cedille-json cedille-json))
+
+;; -----------------------------------------------
+
+;; Generating Mouse-Over Tooltips
+
+#;(define-syntax-parser mouse-over-tooltips
+  [(_ src* ln*:nat col*:nat pos*:nat s*:str)
+   (define src (syntax->datum #'src*))
+   (define ln (syntax-e #'ln*))
+   (define col (syntax-e #'col*))
+   (define pos (syntax-e #'pos*))
+   (define s (syntax-e #'s*))
+   (syntax-property
+    #'(begin)
+    'mouse-over-tooltips
+    (generate-mouse-over-tooltips src ln col pos s))])
+
+#;(begin-for-syntax
+  ;; A MouseOverTooltip is a:
+  ;;   (Vector
+  ;;     ; A syntax object whose `syntax-source` field indicates which
+  ;;     ; file the tooltip goes in
+  ;;     Syntax
+  ;;     ; The start position in the editor (zero-indexed)
+  ;;     Nat
+  ;;     ; The end position in the editor (zero-indexed)
+  ;;     Nat
+  ;;     ; The content of the tooltip, or a function to produce the content
+  ;;     (U String (-> String)))
+
+  ;; generate-mouse-over-tooltips :
+  ;; Any Nat Nat Nat String -> [ConsTreeof MouseOverTooltip]
+  (define (generate-mouse-over-tooltips src ln col pos s)
+    (define tmp (make-temporary-file))
+    (define tmp-out (open-output-file tmp #:exists 'replace))
+    (write-whitespace-toward ln col pos tmp-out)
+    (write-string s tmp-out)
+    (close-output-port tmp-out)
+    ;; ----------
+    (define src-stx (datum->syntax #f #f (list src ln col pos 0)))
+    (define cedille-json (cedille-check tmp))
+    (define spans (hash-ref cedille-json 'spans))
+    (for/list ([span (in-list spans)])
+      (match span
+        [(list desc start end _)
+         (vector-immutable src-stx (sub1 start) (sub1 end) desc)]))))
 
 ;; -----------------------------------------------
 
